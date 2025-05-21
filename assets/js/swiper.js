4a0b7a0b511cd914f46b63179a53cacd980dea1d
eubrunocoelho <eu.brunocoelho94@gmail.com>
2025-05-21 15:10:25 -0300

    feat: add department slider

---
+++ assets/js/swiper.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    () => {
+        let swiper = new Swiper('#departmentsSlider',
+            {
+                slidesPerView: 'auto',
+                loop: false,
+                spaceBetween: 20,
+            }
+        );
+    }
+);
